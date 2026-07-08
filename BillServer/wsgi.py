dce9701b984f8a1cc65ecf82d6cb9ad73d6c66e1
NovaProtocol <novaprotocol@users.noreply.github.com>
2026-07-08 17:23:59 +0800

    compile SCSS at startup in production (wsgi.py) so CSS files exist in Docker

--- BillServer/wsgi.py
+++ BillServer/wsgi.py
@@ -4,6 +4,7 @@ import os
 
 os.environ["DEPLOYMENT_TYPE"] = "PRODUCTION"
 
-from run import _preflight_db, app  # noqa: E402
+from run import _compile_scss, _preflight_db, app  # noqa: E402
 
+_compile_scss(app)
 _preflight_db(app)
